c238af8c0d2b8a9ef562d1f8c485517d76bfe169
omkar2809 <55548995+omkar2809@users.noreply.github.com>
2020-05-05 16:35:19 +0530

    navigation

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,28 +1,67 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
-import { DISHES } from '../shared/dishes';
-import DishDetail from './DishdetailComponent';
-import {View} from 'react-native';
+import Home from './HomeComponent';
+import Dishdetail from './DishdetailComponent';
+import {View, Platform} from 'react-native';
+import { createStackNavigator, createDrawerNavigator} from 'react-navigation';
+
+const MenuNavigator = createStackNavigator({
+    Menu: {screen: Menu},
+    Dishdetail : {screen: Dishdetail}
+}, {
+    initialRouteName: 'Menu',
+    navigationOptions: {
+        headerStyle: {
+            backgroundColor: '#512DA8'
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+            color: '#fff'
+        }
+    }
+});
+
+const HomeNavigator = createStackNavigator({
+    Home: {screen: Home}
+}, {
+    navigationOptions: {
+        headerStyle: {
+            backgroundColor: '#512DA8'
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+            color: '#fff'
+        }
+    }
+});
+
+const MainNavigator = createDrawerNavigator({
+    Home: {
+        screen: HomeNavigator,
+        navigationOptions: {
+            title: 'Home',
+            drawLabel: 'Home'
+        }
+    },
+    Menu: {
+        screen: MenuNavigator,
+        navigationOptions: {
+            title: 'Menu',
+            drawLabel: 'Menu'
+        }
+    }
+}, {
+    drawerBackgroundColor: '#D1C4E9'
+});
 
 class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            dishes: DISHES,
-            selectedDish: null
-        };
-    }
-
-    onDishSelect(dishId){
-        this.setState({selectedDish: dishId})
-    }
+    
     render() {
         
         return (
-            <View style={{flex:1}}>
-            <Menu dishes={this.state.dishes} onPress={(dishId) => this.onDishSelect(dishId)} />
-            <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-        </View>
+            <View style={{flex:1, paddingTop: Platform.os==='ios' ? 0 : Expo.Constants.statusBarHeight}}>
+                <MainNavigator />
+            </View>
             
         );
     }
--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -1,30 +1,48 @@
 import React from 'react';
 import { View, FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
+import { DISHES } from '../shared/dishes';
 
-function Menu(props) {
+class Menu extends React.Component {
 
-    const renderMenuItem = ({item, index}) => {
+    constructor(props){
+        super(props);
+        this.state = {
+            dishes: DISHES
+        };
+    }
 
-        return (
-            <ListItem
+    static navigationOptions = {
+        title: 'Menu'
+    }
+
+    
+    render(){
+        
+        const renderMenuItem = ({item, index}) => {
+            return (
+                <ListItem
                     key={index}
                     title={item.name}
                     subtitle={item.description}
                     hideChevron={true}
-                    onPress={() => props.onPress(item.id)}
+                    onPress={() => navigate('Dishdetail', {dishId: item.id})}
                     leftAvatar={{ source: require('./images/uthappizza.png')}}
-                  />
-        );
-    };
+                />
+            );
+        };
 
-    return (
-        <FlatList 
-            data={props.dishes}
-            renderItem={renderMenuItem}
-            keyExtractor={item => item.id.toString()}
+        const {navigate} = this.props.navigation;
+
+        return (
+            <FlatList 
+                data={this.state.dishes}
+                renderItem={renderMenuItem}
+                keyExtractor={item => item.id.toString()}
             />
-    );
+        );
+    }
+    
 }
 
 
